cf1edfbbc1bb5cf25c63c04ae38ef010c114381b
WillyCuk <willyhalim543@gmail.com>
2024-05-07 19:05:57 +0700

    final UTS

---
+++ lib/theme/light.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+    splashColor: Colors.transparent,
+    highlightColor: Colors.transparent,
+    hoverColor: Colors.transparent,
+    colorScheme: ColorScheme.light(
+      background: Colors.white,
+      primary: Colors.grey.shade500,
+      secondary: Colors.grey.shade100,
+      tertiary: Colors.white,
+      inversePrimary: Colors.grey.shade700,
+    ));
