a789953955cd15b657cdf3d1650f78a988fc1da8
WillyCuk <willyhalim543@gmail.com>
2024-06-19 20:17:41 +0700

    commit 19 juni

--- lib/main.dart
+++ lib/main.dart
@@ -8,28 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:page_transition/page_transition.dart';
 
-// void main() {
-//   runApp(MultiProvider(providers: [
-//     ChangeNotifierProvider(create: (context) => User()),
-//     ChangeNotifierProvider(create: (context) => ItemList()),
-//     ChangeNotifierProvider(create: (context) => UserOrder()),
-//     ChangeNotifierProvider(create: (context) => ThemeProvider()),
-//   ], child: const MyApp()));
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp.router(
-//       theme: Provider.of<ThemeProvider>(context).themeData,
-//       debugShowCheckedModeBanner: false,
-//       routerConfig: MyRouter().router,
-//     );
-//   }
-// }
-
 void main() {
   runApp(MultiProvider(
     providers: [
